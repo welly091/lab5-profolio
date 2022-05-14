291d02e544959bf98afdd4b7ecdc872d0862a9be
welly091 <whtp1490@gmail.com>
2022-05-13 22:05:58 -0700

    add projects display

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
+                  data-icon="twemoji-flag-for-flag-united-states"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
@@ -45,12 +45,7 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
+                
               </div>
             </div>
             <div className="col">
